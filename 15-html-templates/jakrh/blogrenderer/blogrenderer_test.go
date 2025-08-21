c6553b7e7bdc8ffcc3db2adeb78c2e4b38bef5b9
Jack (Lung-Hsuan) Hung <lhhungx@gmail.com>
2025-08-22 01:59:30 +0800

    Add jakrh's code of 15-html-templates

---
+++ 15-html-templates/jakrh/blogrenderer/blogrenderer_test.go
@@ -0,0 +1,63 @@
+package blogrenderer_test
+
+import (
+	"bytes"
+	"io"
+	"testing"
+
+	"github.com/Tech-Book-Community/workshop-learn-go-with-tests/15-html-templates/jakrh/blogrenderer"
+	approvals "github.com/approvals/go-approval-tests"
+)
+
+func TestRender(t *testing.T) {
+	aPost := blogrenderer.Post{
+		Title:       "hello world",
+		Body:        "This is a post",
+		Description: "This is a description",
+		Tags:        []string{"go", "tdd"},
+	}
+
+	postRenderer, err := blogrenderer.NewPostRenderer()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("it converts a single post into HTML", func(t *testing.T) {
+		buf := bytes.Buffer{}
+
+		if err := postRenderer.Render(&buf, aPost); err != nil {
+			t.Fatal(err)
+		}
+
+		approvals.VerifyString(t, buf.String())
+	})
+
+	t.Run("it renders an index of posts", func(t *testing.T) {
+		buf := bytes.Buffer{}
+		posts := []blogrenderer.Post{{Title: "Hello World"}, {Title: "Hello World 2"}}
+
+		if err := postRenderer.RenderIndex(&buf, posts); err != nil {
+			t.Fatal(err)
+		}
+
+		approvals.VerifyString(t, buf.String())
+	})
+}
+
+func BenchmarkRender(b *testing.B) {
+	aPost := blogrenderer.Post{
+		Title:       "hello world",
+		Body:        "This is a post",
+		Description: "This is a description",
+		Tags:        []string{"go", "tdd"},
+	}
+
+	postRenderer, err := blogrenderer.NewPostRenderer()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for b.Loop() {
+		postRenderer.Render(io.Discard, aPost)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Tech-Book-Community/workshop-learn-go-with-tests
 
 go 1.24.3
+
+require (
+	github.com/approvals/go-approval-tests v1.6.0
+	github.com/gomarkdown/markdown v0.0.0-20250810172220-2e2c11897d1a
+)
